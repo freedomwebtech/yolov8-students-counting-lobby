e5fdedc25627e8e84346365f4a86a3a7ea64a2c0
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2023-09-03 13:33:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,12 +27,7 @@ class_list = data.split("\n")
 #print(class_list)
 
 count=0
-persondown={}
-tracker=Tracker()
-counter1=[]
 
-personup={}
-counter2=[]
 
 area1=[(494,289),(505,499),(578,496),(530,292)]
 area2=[(548,290),(600,496),(637,493),(574,288)]
@@ -66,47 +61,9 @@ while True:
         d=int(row[5])
         
         c=class_list[d]
-        if 'person' in c:
-
-            list.append([x1,y1,x2,y2])
-       
         
-    bbox_id=tracker.update(list)
-    for bbox in bbox_id:
-        x3,y3,x4,y4,id=bbox
-        
-        result=cv2.pointPolygonTest(np.array(area2,np.int32),((x4,y4)),False)
-        if result>=0:
-           cv2.circle(frame,(x4,y4),4,(255,0,255),-1)
-           cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1)
-           persondown[id]=((x4,y4))
-        if id in persondown:   
-           result1=cv2.pointPolygonTest(np.array(area1,np.int32),((x4,y4)),False)
-           if result1>=0:
-              cv2.circle(frame,(x4,y4),4,(255,0,255),-1)
-              cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1)
-              if counter1.count(id)==0:
-                  counter1.append(id)
-        result2=cv2.pointPolygonTest(np.array(area1,np.int32),((x4,y4)),False)
-        if result2>=0:
-           cv2.circle(frame,(x4,y4),4,(255,0,255),-1)
-           cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1)
-           personup[id]=((x4,y4))
-        if id in personup:   
-           result3=cv2.pointPolygonTest(np.array(area2,np.int32),((x4,y4)),False)
-           if result3>=0:
-              cv2.circle(frame,(x4,y4),4,(255,0,255),-1)
-              cvzone.putTextRect(frame,f'{id}',(x3,y3),1,1)
-              if counter2.count(id)==0:
-                  counter2.append(id)
 
- 
-    cv2.polylines(frame,[np.array(area1,np.int32)],True,(255,255,255),2)
-    cv2.polylines(frame,[np.array(area2,np.int32)],True,(255,255,255),2)
-    studentsout=len(counter1)
-    studentsin=len(counter2)
-    cvzone.putTextRect(frame,f'StudentsOut:-{studentsout}',(50,60),2,2)
-    cvzone.putTextRect(frame,f'StudentsIn:-{studentsin}',(50,160),2,2)
+   
 
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1)&0xFF==27:
